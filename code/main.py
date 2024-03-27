8ba70cab62b850991b9779d6a1d29b1d1b4251c9
Yasmin Tarek <104358224+YasminTarekk@users.noreply.github.com>
2024-03-27 13:19:56 +0200

    Create main.py

---
+++ code/main.py
@@ -0,0 +1,96 @@
+import math
+import time
+from finalplayer import HumanPlayer,SmartPlayer
+
+
+class TicTacToe():
+    def __init__(self):
+        self.board = self.make_board()
+        self.current_winner = None
+
+    @staticmethod
+    def make_board():
+        return [' ' for i in range(9)]#returns a list of 9 empty spaces
+
+    def print_board(self):
+        for row in [self.board[i*3:(i+1) * 3] for i in range(3)]:
+            print('| ' + ' | '.join(row) + ' |') 
+
+    @staticmethod
+    def print_board_nums():
+        number_board = [[str(i) for i in range(j*3, (j+1)*3)] for j in range(3)]
+        for row in number_board:
+            print('| ' + ' | '.join(row) + ' |')
+
+    def make_move(self, square, letter): #square:index
+        if self.board[square] == ' ':
+            self.board[square] = letter 
+            if self.winner(square, letter):
+                self.current_winner = letter
+            return True
+        return False 
+
+    def winner(self, square, letter):
+        # check the row
+        row_ind = math.floor(square / 3)
+        row = self.board[row_ind*3:(row_ind+1)*3]#bt extracts el values el goa el index el bigebo mn el row_ind
+        if all([s == letter for s in row]):
+            return True
+        col_ind = square % 3
+        column = [self.board[col_ind+i*3] for i in range(3)]
+        if all([s == letter for s in column]):
+            return True
+        if square % 2 == 0:
+            diagonal1 = [self.board[i] for i in [0, 4, 8]]
+            if all([s == letter for s in diagonal1]):
+                return True
+            diagonal2 = [self.board[i] for i in [2, 4, 6]]
+            if all([s == letter for s in diagonal2]):
+                return True
+        return False
+
+    def empty_squares(self): #returns true law fih 3al a2al 1 square fady
+        return ' ' in self.board
+
+    def num_empty_squares(self):#returns 3adad el empty squares
+        return self.board.count(' ')
+
+    def available_moves(self):#returns a list of indeces benesba lel positions el fadya 
+        return [i for i, x in enumerate(self.board) if x == " "]
+
+
+def play(game, x_player, o_player, print_game=True):
+
+    if print_game:
+        game.print_board_nums()
+
+    letter = 'X'
+    while game.empty_squares():
+        if letter == 'O':
+            square = o_player.get_move(game)
+        else:
+            square = x_player.get_move(game)
+        if game.make_move(square, letter):
+
+            if print_game:
+                print(letter + ' makes a move to square {}'.format(square))
+                game.print_board()
+
+            if game.current_winner:#bey-check law had kasab
+                if print_game:
+                    print(letter + ' wins!')
+                return letter  # ends the loop and exits the game
+            letter = 'O' if letter == 'X' else 'X'  # switches player
+
+        time.sleep(.8)
+
+    if print_game:
+        print('It\'s a tie!')
+
+
+
+if __name__ == '__main__':  #creates instances
+    x_player =SmartPlayer('X')
+    o_player =HumanPlayer('O')
+    t = TicTacToe()
+    play(t, x_player, o_player, print_game=True)
